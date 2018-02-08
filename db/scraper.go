8a17b7b27878b4856854c7ec67536ba4cce3f17a
Nick Boughton <nicholasboughton@gmail.com>
2018-02-08 16:26:55 +0000

    tidied scrape

--- db/scraper.go
+++ db/scraper.go
@@ -20,6 +20,8 @@ func Scrape() <-chan Record {
 	c := make(chan Record)
 
 	go func() {
+		defer close(c)
+
 		for year := time.Now().Year(); year >= 1994; year-- {
 			// Get archive page
 			archivePage, err := goquery.NewDocument(fmt.Sprintf(archiveURL, baseURL, year))
@@ -36,57 +38,66 @@ func Scrape() <-chan Record {
 					return
 				}
 
-				// Load results page
-				resultPage, err := goquery.NewDocument(fmt.Sprintf("%s%s", baseURL, resultURL))
+				rec, err := parseResultPage(resultURL)
 				if err != nil {
 					log.Println(err)
 					return
 				}
 
-				// Create new Record
-				rec := NewRecord()
-
-				// Set Record date
-				if rec.Date, err = parseDateFromURL(resultURL); err != nil {
-					log.Println(err)
-					return
-				}
-
-				// Set Record ball results
-				resultPage.Find(".result").Each(func(i int, s *goquery.Selection) {
-					result, err := strconv.Atoi(s.Text())
-					if err != nil {
-						log.Println(err)
-					}
-
-					rec.Ball = append(rec.Ball, result)
-				})
-
-				// Set Record machine and set
-				resultPage.Find("#siteContainer .main .lotto tbody tr td").Each(func(i int, s *goquery.Selection) {
-					if strings.Contains(s.Text(), "Set Used:") {
-						n, err := strconv.Atoi(parseUsed(s.Text()))
-						if err != nil {
-							log.Println(err)
-						}
-
-						rec.Set = n
-					}
-
-					if strings.Contains(s.Text(), "Machine Used:") {
-						rec.Machine = parseUsed(s.Text())
-					}
-				})
-
 				c <- rec
 			})
-
 		}
-
-		close(c)
 	}()
 
 	return c
+}
+
+func parseResultPage(url string) (Record, error) {
+	// Create new Record
+	rec := NewRecord()
+
+	// Load results page
+	resultPage, err := goquery.NewDocument(fmt.Sprintf("%s%s", baseURL, url))
+	if err != nil {
+		log.Println(err)
+		return rec, err
+	}
+
+	// Set Record date
+	if rec.Date, err = parseDateFromURL(url); err != nil {
+		log.Println(err)
+		return rec, err
+	}
+
+	// Set Record ball results
+	resultPage.Find(".result").Each(func(i int, s *goquery.Selection) {
+		result, err := strconv.Atoi(s.Text())
+		if err != nil {
+			log.Println(err)
+		}
+
+		if i < len(rec.Ball) {
+			rec.Ball[i] = result
+		}
+	})
+
+	// Set Record machine and set
+	resultPage.Find("#siteContainer .main .lotto tbody tr td").Each(func(i int, s *goquery.Selection) {
+		if strings.Contains(s.Text(), "Set Used:") {
+			n, err := strconv.Atoi(parseUsed(s.Text()))
+			if err != nil {
+				log.Println(err)
+			}
+
+			rec.Set = n
+		}
+
+		if strings.Contains(s.Text(), "Machine Used:") {
+			rec.Machine = parseUsed(s.Text())
+		}
+	})
+
+	return rec, nil
 }
 
 func parseUsed(str string) string {
